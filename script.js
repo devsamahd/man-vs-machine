17a6fb3e05f9e63b9bacc1dd22f9a675605da228
devsamahd <devsamahd@gmail.com>
2022-09-27 22:14:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+/*
+  Rock Paper Scissors 🚀🔥
+  Concepts covered in this project
+    👉 For loops
+    👉 Dom Manipulation
+    👉 Variables
+    👉 Conditionals (if else if)
+    👉 Template Literals
+    👉 Event Listeners
+    👉 Higher order Function (Math.random())
+*/
+const Total = {computerScore: 0, playerScore:0}
+// ** getComputerChoice randomly selects between `rock` `paper` `scissors` and returns that string **
+// getComputerChoice() 👉 'Rock'
+// getComputerChoice() 👉 'Scissors'
+function getComputerChoice() {
+  rps = ['rock','paper','scissors']
+  let randomchoice = Math.floor(Math.random()*3)
+  return rps[randomchoice]
+}
+
+// ** getResult compares playerChoice & computerChoice and returns the score accordingly **
+// human wins - getResult('Rock', 'Scissors') 👉 1
+// human loses - getResult('Scissors', 'Rock') 👉 -1
+// human draws - getResult('Rock', 'Rock') 👉 0
+function getResult(playerChoice, computerChoice) {
+  // return the result of score based on if you won, drew, or lost
+  
+  let score;
+
+  // All situations where human draws, set `score` to 0
+  if(playerChoice == computerChoice){
+    score = 0;
+  }
+
+  // All situations where human wins, set `score` to 1
+  // make sure to use else ifs here
+  else if(playerChoice== 'scissors' && computerChoice == 'paper'){
+    return score = 1;
+  }
+  else if(playerChoice== 'Rock' && computerChoice == 'Scissors'){
+    return score = 1;
+  }
+  else if(playerChoice== 'paper' && computerChoice == 'rock'){
+    return score = 1;
+  }
+
+  // Otherwise human loses (aka set score to -1)
+  
+  else{
+    score = -1
+  }
+  // return score
+  return score;
+}
+
+// ** showResult updates the DOM to `You Win!` or `You Lose!` or `It's a Draw!` based on the score. Also shows Player Choice vs. Computer Choice**
+function showResult(score, playerChoice, computerChoice) {
+  // Hint: on a score of -1
+  // You should do result.innerText = 'You Lose!'
+  // Don't forget to grab the div with the 'result' id!
+  let resultview = document.getElementById("result")
+  let handsdiv = document.getElementById("hands")
+  let playerscore = document.getElementById("player-score")
+  
+  if(score == -1){
+    resultview.innerText = `You lose`
+  }
+  else if(score == 1){
+    resultview.innerText = `You win`
+  }
+  else{
+    resultview.innerText = `Draw`
+  }
+  handsdiv.innerText =`👱 ${playerChoice} vs 🤖 ${computerChoice} `
+  playerscore.innerText= 'Your score: ' + Total["playerScore"]
+}
+
+// ** Calculate who won and show it on the screen **
+function onClickRPS(playerChoice) {
+  const computerchoice = getComputerChoice()
+  let score = getResult(playerChoice, computerchoice)
+  Total['playerScore'] += score
+  showResult(score, playerChoice, computerchoice)
+}
+
+
+// ** Make the RPS buttons actively listen for a click and do something once a click is detected **
+function playGame() {
+  // use querySelector to select all RPS Buttons
+  const rpsbtn = document.querySelectorAll(".rpsButton")
+  // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
+  
+  // 1. loop through the buttons using a forEach loop
+  // 2. Add a 'click' event listener to each button
+  // 3. Call the onClickRPS function every time someone clicks
+  // 4. Make sure to pass the currently selected rps button as an argument
+
+ rpsbtn.forEach(btn => {
+  btn.onclick = () => onClickRPS(btn.value)
+ })
+
+  // Add a click listener to the end game button that runs the endGame() function on click
+  document.getElementById("endGameButton").onclick = () => endGame()
+  
+}
+
+// ** endGame function clears all the text on the DOM **
+function endGame() {
+  Total["playerScore"] = 0
+  
+  document.getElementById("result").innerHTML = ''
+  document.getElementById("hands").innerHTML = ''
+  document.getElementById("player-score").innerHTML = '' 
+  
+}
+
+playGame()
